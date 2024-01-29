19d529c2b0858abbb5336fb31a5ef55efc28722d
mubeenamin <mubeenamin6@gmail.com>
2024-01-29 12:34:17 +0500

    updated

---
+++ streamlit/hello/hello.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+# st.text('Fixed width text')
+# st.markdown('# _Markdown_') # see *
+# st.latex(r''' e^{i\pi} + 1 = 0 ''')
+# st.write('Most objects') # df, err, func, keras!
+# st.write(['st', 'is <', 3]) # see *
+# st.title('My title')
+# st.header('My header')
+# st.subheader('My sub')
+# st.code('for i in range(8): foo()')
+
+
+
+# st.balloons()
+# st.snow()
+# st.toast('Warming up...')
+st.error('Error message')
+st.warning('Warning message')
+st.info('Info message')
+st.success('Success message')
+st.exception(e)
